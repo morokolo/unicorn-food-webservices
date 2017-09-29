4d4ee47849f41cd82928510f3a76a888d8e4ace6
Fauz O. Ecum <fauz.ecum@foecum.com>
2017-09-29 14:26:31 +0200

    Added use registration

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,19 @@
 from rest_framework import serializers
 from .models import *
 
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'username', 'last_name', 'email', 'password')
+
+    def create(self, validated_data):
+        user = super(UserSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,52 @@
 from django.shortcuts import render
 
 from .serializers import *
-from rest_framework import viewsets
-
+from rest_framework import views, viewsets
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
 # ViewSets define the view behavior.
+
+@api_view(['POST'])
+@permission_classes((AllowAny,))
+def create_user(request):
+    serialized = UserSerializer(data=request.data['user'])
+    if serialized.is_valid():
+        serialized.save()
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
+# def create_users(request):
+#     data = self.request.data
+#     user = User()
+#
+#     user.username = data['username']
+#     user.email = data['email']
+#     user.first_name = data['first_name']
+#     user.last_name = data['last_name']
+#     user.save()
+#
+#     user.set_password(data['password'])
+#     user.save()
+#     user_profile = UserProfile()
+#     user_profile.user = user
+#     user_profile.user_type = 'buyer'
+#     user_profile.cellphone = data['cellphone']
+#     image = data['image']
+#
+#     user_profile.save()
+#
+#     return Response(json.dumps(user_profile))
+
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+    parser_classes = (MultiPartParser, FormParser,)
+
+    #def perform_create(self, serializer):
+
 
 
 class RestaurantViewSet(viewsets.ModelViewSet):
