0adb96785cdce8171caa7596894a336ff66696fa
Fauz O. Ecum <fauz.ecum@foecum.com>
2017-09-29 16:25:48 +0200

    Update registration service

---
+++ menu/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from menu import views
+
+urlpatterns = [
+    url(r'restaurant/(?P<restaurant>[0-9]+)/items$', views.MenuItemsByRestaurantId.as_view(), name='menu_items'),
+]
+
+
