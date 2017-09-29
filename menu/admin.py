ec1c1cbcaffa7c77700f6c1be9deacf26141413d
Mokgadi rasekgala <rasekgalam@gmail.com>
2017-09-29 12:34:43 +0200

    Menu Models

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+
+
+class MenuAdmin(admin.ModelAdmin):
+    model = Menu
+    list_display = ('restuarant',)
+
+
+class MenuCategoryAdmin(admin.ModelAdmin):
+    model = MenuCategory
+    list_display = ('title', 'menu',)
+
+
+class MenuItemAdmin(admin.ModelAdmin):
+    model = MenuItem
+    list_display = ('user_profile', 'restaurant')
+
+
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(MenuCategory, MenuCategoryAdmin)
+admin.site.register(MenuItem, MenuItemAdmin)
